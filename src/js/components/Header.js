b7cdbd98d80a8453ec8124afbbf3b9722b4e90af
Michael Korzun <michaelkorzun@Michaels-MacBook-Pro-3.local>
2017-11-01 15:48:05 +0200

    header

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,8 +4,13 @@ export default class Header extends React.Component {
 
     render() {
         return (
-            <div className="wrapper">
-              Header
+            <div className="header">
+              <a href="#"><div className="logo">logo here</div></a>
+              <input ref="Search" placeholder="Search..." />
+              <div className="cart">Cart is here</div>
+              <div className="account">Accaunt is here</div>
+              <div className="accountStatus">Accaunt status is here</div>
+
             </div>
         )
     }
