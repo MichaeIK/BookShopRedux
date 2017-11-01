98a374216b3cfe09cc6d3095880a0d1b68d48656
matveychuk <matveychuk.a@gmail.com>
2017-11-01 15:48:18 +0200

    images

--- src/js/components/MainLayout.js
+++ src/js/components/MainLayout.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-
+import {PATH} from '../config';
+// import pict from ''
+// console.log(PATH)
+// const x = `.${PATH}heart.png`;
 export default class MainLayout extends React.Component {
 
     render() {
+        // console.log(`${PATH}heart.png`)
         return (
-            <div className="wrapper">
-                <Header />
-                {this.props.children}
-                <Footer />
+            <div className="container">
+                <div className="row">
+                    <Header />
+                </div>
+                <div className="row">
+                    {this.props.children}
+                </div>
+                <div className="row">
+                    <Footer />
+                </div>
+
+                <div style={{backgroundImage: 'url(./assets/img/heart.png)',height:'300px'}}></div>
+
+
             </div>
         )
     }
