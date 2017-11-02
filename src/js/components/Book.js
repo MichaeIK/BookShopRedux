c95d2de4939da5773bdae14f02232992259f3167
matveychuk <matveychuk.a@gmail.com>
2017-11-02 18:57:25 +0200

    state

--- src/js/components/Book.js
+++ src/js/components/Book.js
@@ -8,7 +8,7 @@ import { withRouter } from 'react-router';
 import Slider from './Slider';
 import { ORIGIN, ENV_HREF } from '../config';
 const mapStateToProps = (state, ownProps) => {
-   return { books: state.data }
+    return { books: state.data }
 }
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({ fetchBooks, addToHistory }, dispatch);
@@ -16,7 +16,7 @@ const mapDispatchToProps = (dispatch) => {
 @withRouter
 @connect(mapStateToProps, mapDispatchToProps)
 export default class Book extends React.Component {
-   
+
     fetchData = (keyword) => {
         // console.log(this.props)
         keyword = keyword ? keyword : this.props.match.params.category ? this.props.match.params.category : 'books for developer';
@@ -41,24 +41,41 @@ export default class Book extends React.Component {
         // console.log(this.props.history);
         this.props.addToHistory(id);
         this.props.history.push(`/book/${id}`);
-        
+
 
     }
     renderBooks = (item, index) => {
         let url = {
             backgroundImage: `url(${item.volumeInfo.imageLinks.smallThumbnail})`
+        };
+        let urlPrice = {
+            backgroundImage: 'url(../../assets/img/price.png)'
         }
-       
+        let urlCart = {
+            backgroundImage: 'url(../../assets/img/icons8-buy.png)'
+        }
+
         return (
             <div key={index} className="col-sm-6 col-md-3 book-wrapper" onClick={this.handleClick.bind(null, item.id)}>
-                <div className="book-card-top">
-                    <div className="col-sm-10 book-image"
-                        style={url}></div>
-                    <div className="col-sm-2 book-stars"></div>
+                <div className="book-image" style={url}></div>
+                <div className="middle-layer"></div>
+                <div className="book-info">
+                    <div className="book-title toggle-info">{item.volumeInfo.title}</div>
+                    <div className="book-pages toggle-info">Pages: {item.volumeInfo.pageCount}</div>
+                    <div className="book-category toggle-info">Category: {item.volumeInfo.categories[0]}</div>
+                    <div className="col-sm-2 book-stars toggle-info"></div>
+                    <div className="book-card-footer toggle-info">
+                        <div className="price-block">
+                            <div className="price-image" style={urlPrice}></div>
+                            <div className="price-value">500 UAH</div>
+                        </div>
+                        <div className="cart-block">
+                            <div className="price-image" style={urlCart}></div>
+                            <div className="price-value">BUY</div>
+                        </div>
+                    </div>
                 </div>
-                <div className="book-title">{item.volumeInfo.title}</div>
-                <div className="book-pages"></div>
-                <div className="book-category"></div>
+
             </div>
         )
     }
