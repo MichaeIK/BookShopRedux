ed1d50a333e3eba87aded65975fcc7c4b935bbb0
Michael Korzun <michaelkorzun@Michaels-MacBook-Pro-3.local>
2017-11-02 14:00:15 +0200

    login1

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Login extends React.Component {
+
+	handleOnClickLogin = ()=> {
+
+	}
 	render() {
 		return (
 			<div className="authorizationForm">
                 <input type="text" ref="login" placeholder="Login" />
                 <input type="password" ref="password" placeholder="Password" />
-                <button className="btn-success" onClick={this.handleonClickLogin}>Login</button>
-                <button className="btn-success" onClick={this.handleonClickLogin}>Sign in</button>
+                <button className="btn-success" onClick={this.handleOnClickLogin}>Login</button>
+                <button className="btn-success" onClick={this.handleOnClickLogin}>Sign in</button>
             </div>
 		);
 	}
